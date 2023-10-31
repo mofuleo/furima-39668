c0c1913aafdc89cee6aeecda8af95836a5035037
mofuleo <masakoterui12@gmail.com>
2023-10-31 20:18:29 +0900

    新規登録のバリデーションの記載

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
     validates :nickname, presence: true
-    validates :last_name, presence: true
-    validates :first_name, presence: true
-    validates :last_name_kana, presence: true
-    validates :first_name_kana, presence: true
+    validates :last_name, format: {with:/\A([ぁ-んァ-ン一-龥]|ー)+\z/     },presence: true
+    validates :first_name, format: {with:/\A([ぁ-んァ-ン一-龥]|ー)+\z/     },presence: true
+    validates :last_name_kana, format: {with:/\A[ァ-ヶー－]+\z/     },presence: true
+    validates :first_name_kana, format: {with:/\A[ァ-ヶー－]+\z/     },presence: true
     validates :birthday, presence: true
 
     has_many :items
