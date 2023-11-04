2314b35e614d99c3acae00ef16b05c819e24e133
mofuleo <masakoterui12@gmail.com>
2023-11-05 01:01:38 +0900

    indexページに商品を表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,8 @@ class Item < ApplicationRecord
   validates :shipping_fee_id,      presence: true
   validates :prefecture_id,        presence: true
   validates :scheduled_delivery_id,presence: true
-  validates :item_price,           presence: true
+  validates :item_price,           presence: true, numericality:{ only_integer: true,greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999, 
+    message: "Out of setting range"}
   validates :image,                presence: true
 
   
