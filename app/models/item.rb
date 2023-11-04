118b4e2632ef0b96f9fc23570b436551486b8a88
mofuleo <masakoterui12@gmail.com>
2023-11-04 22:31:41 +0900

    ActiveHashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,13 +3,28 @@ class Item < ApplicationRecord
   validates :item_name,            presence: true
   validates :item_info,            presence: true
   validates :category_id,          presence: true
-  validates :condition_name,       presence: true
+  validates :condition_id,         presence: true
   validates :shipping_fee_id,      presence: true
   validates :prefecture_id,        presence: true
   validates :scheduled_delivery_id,presence: true
   validates :item_price,           presence: true
   validates :image,                presence: true
 
+  
+extend ActiveHash::Associations::ActiveRecordExtensions
+belongs_to_active_hash :category
+belongs_to_active_hash :condition
+belongs_to_active_hash :shipping_fee
+belongs_to_active_hash :prefecture
+belongs_to_active_hash :scheduled_delivery
+
+  validates :category_id,           numericality:  { other_than: 1,message:"can't be blank" } 
+  validates :condition_id,          numericality:  { other_than: 1,message:"can't be blank" } 
+  validates :shipping_fee_id,       numericality:  { other_than: 1,message:"can't be blank" } 
+  validates :prefecture_id,         numericality:  { other_than: 1,message:"can't be blank" } 
+  validates :scheduled_delivery_id, numericality:  { other_than: 1,message:"can't be blank" } 
+
   belongs_to :user
   has_one_attached :image
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   root to: 'items#index'
-  resources :items
+  resources :items, only:[:index,:new,:create]
 
 end
