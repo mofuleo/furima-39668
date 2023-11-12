1a8caf48b450db8cd1bcec141b01aab54b91f637
mofuleo <masakoterui12@gmail.com>
2023-11-12 17:21:53 +0900

    削除者条件追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :user_permission, only: [:edit, :update]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
@@ -35,7 +35,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
+    @item.destroy if current_user.id == @item.user_id
     redirect_to root_path
   end
 
