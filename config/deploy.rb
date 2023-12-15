a129474f3727ebd13261a53e4f7556d30606bc1b
mofuleo <masakoterui12@gmail.com>
2023-12-15 22:13:33 +0900

    Capri設定

---
+++ config/deploy.rb
@@ -0,0 +1,36 @@
+server '13.114.1.83', user: 'ec2-user', roles: %w{app db web}
+
+# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
+lock '3.18.0'
+
+# Capistranoのログの表示に利用する
+set :application, 'furima-39668'
+
+# どのリポジトリからアプリをpullするかを指定する
+set :repo_url,  'git@github.com:mofuleo/furima-39668.git'
+set :branch, 'main'
+
+# バージョンが変わっても共通で参照するディレクトリを指定
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '3.2.0
+
+# どの公開鍵を利用してデプロイするか
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/mofutomama.pem'] 
+
+# プロセス番号を記載したファイルの場所
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+# Unicornの設定ファイルの場所
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+# デプロイ処理が終わった後、Unicornを再起動するための記述
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
